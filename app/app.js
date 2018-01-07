496c32d81f8c621eb18fbc6b0f76c5c7f7ce8e29
dashrath <dashdj.singh@gmail.com>
2018-01-07 20:04:49 +0530

    first commit
    first commit

---
+++ app/app.js
@@ -0,0 +1,80 @@
+﻿
+var app = angular.module('POC', ['ngRoute', 'ngSanitize','POC1']);
+
+app.config(function ($routeProvider) {
+
+  
+
+    $routeProvider.when("/login", {
+        controller: "indexController",
+        templateUrl: "app/views/login.html"
+    });
+
+    
+
+    $routeProvider.when("/home", {
+        controller: "indexController",
+        templateUrl: "app/views/home.html"
+    });
+
+    $routeProvider.when("/about", {
+        controller: "indexController",
+        templateUrl: "app/views/about.html"
+    });
+
+
+    $routeProvider.when("/pricing", {
+        controller: "indexController",
+        templateUrl: "app/views/pricing.html"
+    });
+
+    //$routeProvider.when("/dashboard", {
+    //    controller: "indexController",
+    //    templateUrl: "app/views/dashboard.html"
+    //});
+
+    //$routeProvider.when("/setting", {
+    //    controller: "indexController",
+    //    templateUrl: "app/views/setting.html"
+    //});
+
+ 
+
+    
+
+    $routeProvider.otherwise({ redirectTo: "/login" });
+
+});
+
+
+
+var app2 = angular.module('POC1', ['ngRoute', 'ngSanitize']);
+
+app2.config(function ($routeProvider) {
+
+
+
+  
+
+    $routeProvider.when("/dashboard", {
+        controller: "indexController",
+        templateUrl: "app/views/dashboard.html"
+    });
+
+    $routeProvider.when("/setting", {
+        controller: "indexController",
+        templateUrl: "app/views/setting.html"
+    });
+
+
+
+
+
+    $routeProvider.otherwise({ redirectTo: "/login" });
+
+});
+
+
+
+
+
---
+++ app/controllers/indexController.js
@@ -0,0 +1,61 @@
+﻿'use strict';
+app.controller('indexController', ['$scope', '$location', function ($scope, $location) {
+
+
+
+    $scope.logindata = { Username: "Admin", Password: "Admin" }
+
+    $scope.Isauthorize = "false";
+    $scope.Isauthorize = localStorage.getItem("Authenticate");
+
+
+
+
+    $scope.login = function () {
+
+        if ($scope.logindata.Username == "Admin" && $scope.logindata.Password == "Admin") {
+
+            $location.path('/dashboard');
+
+            setTimeout(function () {
+                window.location.reload();
+            }, 100)
+
+
+            localStorage.setItem("Authenticate", "true");
+            // Retrieve
+            $scope.Isauthorize = localStorage.getItem("Authenticate");
+
+
+
+
+        }
+
+        else {
+            alert("Invalid Username or Password");
+        }
+
+    }
+
+    $scope.logout = function () {
+        localStorage.setItem("Authenticate", "false");
+        $scope.Isauthorize = localStorage.getItem("Authenticate");
+        $location.path('/login');
+    }
+
+
+
+    $scope.$on("$locationChangeStart", function (event, next, current) {
+        var Isauthorize = localStorage.getItem("Authenticate");
+        var _requestedPath = $location.path();
+        if (Isauthorize != "true") {
+            if (_requestedPath != "/about" && _requestedPath != "/home" && _requestedPath != "/pricing") {
+
+                $location.path("/login");
+            }
+        }
+
+    });
+
+
+}]);
